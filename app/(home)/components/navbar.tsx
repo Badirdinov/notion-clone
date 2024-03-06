94423d10a13768fd36947372be078935f09a76ca
Azizbek <122142907+Badirdinov@users.noreply.github.com>
2024-03-06 10:05:14 +0500

    sdcads

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,11 +1,22 @@
+'use client'
+
 import React from 'react';
 import { Logo } from "@/app/(home)/components/logo";
 import { ModeToggle } from "@/components/shared/ModeToggle";
 import { Button } from "@/components/ui/button";
+import { useScrolled } from "@/hooks/use-scrolled";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
+    
+    const scrolled = useScrolled()
+    
+    
+    
     return (
-      <div className='z-50 bg-background fixed top-0 flex items-center w-full p-6 justify-between'>
+      <div className={ cn('z-50 bg-background fixed top-0 flex items-center w-full p-6 justify-between',
+        scrolled && 'border-b shadow-sm',
+        ) }>
         <Logo/>
           <div className='flex items-center gap-x-2'>
               <Button size={"sm"} variant={"ghost"} >
